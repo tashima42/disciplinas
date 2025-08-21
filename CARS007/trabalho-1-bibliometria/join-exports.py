cddec6fe985bcedf49325e9c40c23db3ddbf34b6
Pedro Tashima <23709916+tashima42@users.noreply.github.com>
2025-08-21 12:50:08 -0300

    add join files script and countries plot

---
+++ CARS007/trabalho-1-bibliometria/join-exports.py
@@ -0,0 +1,17 @@
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+file_count = int(config.get("WOS_FILE_COUNT"))
+output_file = config.get("WOS_DATA")
+
+# create a file called "savedrecs.txt" by joining all files "savedrecs-{i}.txt" where i is from 1 to file_count
+with open(output_file, "w") as outfile:
+    for i in range(1, file_count + 1):
+        with open(f"data/savedrecs-{i}.txt", "r") as infile:
+            infile_lines = infile.read().splitlines(True)
+            if i > 1:
+                infile_lines = infile_lines[1:] # Skip the first line (header) of each file except the first one
+            outfile.writelines(infile_lines)
+
+    print(f"Joined {file_count} files into {output_file}")
